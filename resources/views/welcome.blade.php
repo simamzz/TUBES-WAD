8545d1279658b9a20e3704c07dd25a841ac23c4a
ikhwanamrdn <wanmir213@gmail.com>
2024-12-16 12:40:41 +0700

    comit 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome</title>
+    <title>Tencom</title>
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <style>
         body {
