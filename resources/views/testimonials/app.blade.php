96684f20e12d36a2b58a84d8e79959fcf0086d19
ceriaindahjelita <ceriaindahjelita@gmail.com>
2024-12-30 18:04:05 +0700

    push update index

--- resources/views/testimonials/app.blade.php
+++ resources/views/testimonials/app.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="#">Testimonial</a>
@@ -26,10 +27,12 @@
         </div>
     </nav>
 
+    <!-- Main Content -->
     <div class="container mt-4">
         @yield('content')
     </div>
 
+    <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
--- resources/views/testimonials/index.blade.php
+++ resources/views/testimonials/index.blade.php
@@ -6,39 +6,104 @@ use Illuminate\Support\Facades\Session;
 @endphp
 
 @section('content')
-    <h1>List of Testimonial</h1>
-    <a href="{{ route('testimonials.create') }}" class="btn btn-primary mb-3">Add Testimonial</a>
+<x-slot name="header">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>List of Testimonials</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <style>
+            .custom-container {
+                margin-left: 50px;
+                margin-right: auto;
+            }
+        </style>
+    </head>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>User ID</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Testimonial</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($testimonials as $testimonial)
+    <body>
+        <div class="container custom-container">
+            <div class="row">
+                <div class="col-md-10">
+                    <h3 class="mt-4">List of Testimonials</h3>
+                </div>
+
+                <div class="col-sm-2" style="margin-top: 20px;">
+                    <a class="btn btn-primary" href="{{ route('testimonials.create') }}">Add Testimonial</a>
+                </div>
+            </div>
+        <br>
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>        
+            </div>
+        @endif
+
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>{{ $testimonial->id }}</td>
-                    <td>{{ $testimonial->user_id }}</td>
-                    <td>{{ $testimonial->name }}</td>
-                    <td>{{ ucfirst($testimonial->category) }}</td>
-                    <td>{{ Str::limit($testimonial->testimonial) }}</td>
-                    <td>
-                        <a href="{{ route('testimonials.show', $testimonial) }}" class="btn btn-info btn-sm">Show</a>
-                        <a href="{{ route('testimonials.edit', $testimonial) }}" class="btn btn-warning btn-sm">Edit</a>
-                        <form action="{{ route('testimonials.destroy', $testimonial) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>
-                    </td>
+                    <th scope="col">NO</th>
+                    <th scope="col">User ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Testimonial</th>
+                    <th scope="col">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse ($testimonials as $testimonial)
+                    <tr>
+                        <td><b>{{ $loop->iteration }}.<b></td>
+                        <td>{{ $testimonial->user_id }}</td>
+                        <td>
+                            <a href="{{ route('testimonials.show', $testimonial) }}" style="font-weight: bold; text-decoration: none;">
+                                {{ $testimonial->name }}
+                            </a>
+                        </td>
+                        <td>{{ ucfirst($testimonial->category) }}</td>
+                        <td>{{ Str::limit($testimonial->testimonial, 50) }}</td>
+                        <td>
+                            <a class="btn btn-sm btn-success" href="{{ route('testimonials.edit', $testimonial) }}">Edit</a>
+                            <form action="{{ route('testimonials.destroy', $testimonial) }}" method="POST" style="display:inline;" onsubmit="return confirmDelete(event)">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                            </form>
+
+                            <script>
+                                function confirmDelete(event) {
+                                    // Prevent form submission initially
+                                    event.preventDefault();
+                                    
+                                    // Display the confirmation dialog
+                                    if (confirm("Are you sure you want to delete this testimonial?")) {
+                                        // If user clicks OK, submit the form
+                                        event.target.submit();
+                                    } else {
+                                        // If user clicks Cancel, do nothing
+                                        return false;
+                                    }
+                                }
+                            </script>
+
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-center">No testimonials available. Please add a testimonial.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</body>
 @endsection
+
+<style>
+    .showFile {
+        width: 100px;
+        height: 100px;
+        margin: auto;
+    }
+</style>
